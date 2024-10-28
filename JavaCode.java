c389ac10558ef199361704b6dca34af329f76f99
Ali Vaziri <vaziri1378@gmail.com>
2024-10-27 22:34:18 -0400

    Initial commit of Java code

--- JavaCode.java
+++ JavaCode.java
@@ -1,9 +1,15 @@
 public class JavaCode {
     public int add(int a, int b) {
-    return a + b;
-}
+        return a + b;
+    }
 
     public int subtract(int a, int b) {
         return a - b;
+    }
+
+    public static void main(String[] args) {
+        JavaCode calculator = new JavaCode();
+        System.out.println("Addition: " + calculator.add(5, 3));
+        System.out.println("Subtraction: " + calculator.subtract(5, 3));
     }
 }
